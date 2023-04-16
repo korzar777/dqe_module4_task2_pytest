1ad7f5a5115ffb6adcf7c561fd0d7f3df5f279a2
Aliaksandr_Shkuratau <Aliaksandr_Shkuratau@epam.com>
2023-04-16 13:10:44 +0300

    cicd nodule minor prelim changes

--- config/dbvariables.py
+++ config/dbvariables.py
@@ -1,7 +1,7 @@
 
 #default database configuration - should be used in config file or db, may be encripted
 def db_variables(key=None):
-    variables = {'server':  'EPBYMINW09DC\\SQLEXPRESS',  #'localhost',
+    variables = {'server':  'EPBYMINW09DC\SQLEXPRESS',  #'localhost',
                  'port': '1433', # '1433', #'57175',
                  'database': 'TRN',
                  'username': 'DQTestUser',
